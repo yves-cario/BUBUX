53e273f27b0b8a29b6e60e1cf80a03cac09ca51f
Jeff <jean.francois.jung@gmail.com>
2013-07-27 21:36:52 +0200

    ninja commit from heel, now with templates

--- ged/models.py
+++ ged/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Document:
+class Document(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     version = models.IntegerField()
--- ged/views.py
+++ ged/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from ged.models import Document
 
 def index(request):
-    return HttpResponse("Where is your god now ?")
+    documents = Document.objects.all()
+    template = loader.get_template('ged/index.html')
+    context = RequestContext(request, {
+        'documents': documents
+    })
+
+    return HttpResponse(template.render(context))
