e78cf88740058c79d7f74b8320c9a25df7b49962
Jeff <jean.francois.jung@gmail.com>
2013-07-27 21:22:07 +0200

    titre ? title !

--- ged/models.py
+++ ged/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Document:
-    titre = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     version = models.IntegerField()
 
